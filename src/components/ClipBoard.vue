5f761d9b921383b8b0ad118a6bbe399c2313cc6a
lem <11597313+lemC@user.noreply.gitee.com>
2024-04-20 20:03:04 +0800

    chore: ui antd

---
+++ src/components/ClipBoard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>111</div>
+</template>
+
+<style scoped>
+
+</style>
